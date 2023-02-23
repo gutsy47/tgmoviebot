8304ef960880133e97368ac6c62a5dc0d490606b
gutsy47 <bestcom5055@gmail.com>
2023-02-23 22:12:18 +0300

    Improve: Echo-bot via aiogram
    Added temporary code for future bot
    
    Signed-off-by: gutsy47 <bestcom5055@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+from aiogram import Bot, Dispatcher, executor, types
+import os
+
+# Get data from the environment
+token = os.environ['BOT_KEY']
+
+# Init Bot and Dispatcher
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hello, world!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
